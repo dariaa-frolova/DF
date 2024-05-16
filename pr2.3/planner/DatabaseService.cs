7a9a53272d4d52994aad359cf628b6ee7b95ba21
dariaa-frolova <153801440+dariaa-frolova@users.noreply.github.com>
2024-05-17 01:37:22 +0700

    Add files via upload

---
+++ pr2.3/planner/DatabaseService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Npgsql;
+
+namespace planner;
+
+public static class DatabaseService
+{
+    private static NpgsqlConnection? _connection;
+
+    private static string GetConnectionString()
+    {
+        return @"Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=";
+    }
+
+    public static NpgsqlConnection GetSqlConnection()
+    {
+        if (_connection is null)
+        {
+            _connection = new NpgsqlConnection(GetConnectionString());
+            _connection.Open();
+        }
+
+        return _connection;
+    }
+}
